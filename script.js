52769b493f1c2c95cfc88bfde116ba58f6d55972
Greg <greg19850@gmail.com>
2022-07-24 16:42:18 +0100

    created changeWallet function

--- script.js
+++ script.js
@@ -8,5 +8,19 @@ class Wallet {
       if (_money >= bidValue) return true;
       return false;
     };
+
+    this.changeWallet = (value, type = '+') => {
+      if (typeof value === 'number' && !isNaN(value)) {
+        if (type === '+') {
+          return (_money += value);
+        } else if (type === '-') {
+          return (_money -= value);
+        } else {
+          alert('Incorrect type of action!');
+        }
+      } else {
+        throw new Error('Incorrect number');
+      }
+    };
   }
 }
